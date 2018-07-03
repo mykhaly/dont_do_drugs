d727baba31e58c6e9f2a44ebe6cc02673706ac4e
Yurii Mykhalchuk <yurkamykhalchuk@gmail.com>
2018-07-03 20:07:45 +0300

    Made main() parameterized

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
@@ -20,8 +22,16 @@ def plot_stats(epoch_count, stats):
     plt.show()
 
 
-def main():
-    df = pd.read_csv('data/data_cannabis_2.csv')
+def main(dataset_filename,
+         input_dim,
+         first_hid_dim,
+         second_hid_dim,
+         output_dim,
+         learning_rate,
+         batch_size,
+         number_of_iterations):
+    data_dir = 'data'
+    df = pd.read_csv(os.path.join(data_dir, dataset_filename))
     x = df[['age', 'gender', 'education', 'country', 'ethnicity', 'nscore', 'escore', 'oscore',
             'ascore', 'cscore', 'impulsive', 'ss']]
     y = df['cannabis']
@@ -29,18 +39,23 @@ def main():
     y = np.array(y)
     n_samples = x.shape[0]
     n_train = int(n_samples * 0.7)
-    data_x_train, data_y_train = x[0:n_train, :], y[:n_train]
-    data_x_test, data_y_test = x[n_train:, :], y[n_train:]
+
+    perm = np.random.permutation(n_samples)
+    train_indx = perm[:n_train]
+    test_indx = perm[n_train:]
+
+    data_x_train, data_y_train = x[train_indx, :], y[train_indx]
+    data_x_test, data_y_test = x[test_indx, :], y[test_indx]
+
     data_x_train = data_x_train.T
     data_x_test = data_x_test.T
 
-    number_of_iterations = 600
-    nn = MLP(input_dim=12,
-             first_hid_dim=25,
-             second_hid_dim=15,
-             output_dim=2,
-             learning_rate=1e-3,
-             batch_size=20)
+    nn = MLP(input_dim=input_dim,
+             first_hid_dim=first_hid_dim,
+             second_hid_dim=second_hid_dim,
+             output_dim=output_dim,
+             learning_rate=learning_rate,
+             batch_size=batch_size)
 
     stats = nn.train(data_x_train, data_y_train, data_x_test, data_y_test, number_of_iterations)
 
@@ -54,4 +69,11 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(dataset_filename='data_cannabis_2.csv',
+         input_dim=12,
+         first_hid_dim=25,
+         second_hid_dim=15,
+         output_dim=2,
+         learning_rate=1e-3,
+         batch_size=20,
+         number_of_iterations=600)
